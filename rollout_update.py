1a9012465b1678145f7e0dd01ef13b5bfcda0bcd
Angel G. Olloqui <angelgarcia.mail@gmail.com>
2021-12-30 14:02:08 +0100

    Added code to update rollout

---
+++ rollout_update.py
@@ -0,0 +1,70 @@
+
+import copy
+import sys
+import httplib2
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.client import AccessTokenRefreshError
+
+TRACK = (
+    'production'
+)
+
+def main():
+  PACKAGE_NAME = sys.argv[1]
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    '/Users/agarcia/Downloads/Google Play Android Developer-53dbcb56fbbf.json',
+    scopes='https://www.googleapis.com/auth/androidpublisher')
+
+  http = httplib2.Http()
+  http = credentials.authorize(http)
+
+  service = build('androidpublisher', 'v3', http=http)
+
+  try:
+    edit_request = service.edits().insert(body={}, packageName=PACKAGE_NAME)
+    result = edit_request.execute()
+    edit_id = result['id']
+
+    track_result = service.edits().tracks().get(editId=edit_id, packageName=PACKAGE_NAME, track=TRACK).execute()
+    old_result = copy.deepcopy(track_result)
+
+    print("Current status: ", track_result)
+    for release in track_result['releases']:
+        if 'userFraction' in release:
+            rolloutPercentage = release['userFraction']
+            if rolloutPercentage == 0:
+                print('Release not rolled out yet')
+                continue
+            elif rolloutPercentage < 0.02:
+                release['userFraction'] = 0.02                         
+            elif rolloutPercentage < 0.05:
+                release['userFraction'] = 0.05
+            elif rolloutPercentage < 0.1:
+                release['userFraction'] = 0.1
+            elif rolloutPercentage < 0.2:
+                release['userFraction'] = 0.2
+            elif rolloutPercentage < 0.5:
+                release['userFraction'] = 0.5
+            elif rolloutPercentage < 1.0:
+                release['userFraction'] = 1.0
+                release['status'] = 'completed'
+            else:
+                print('Release already fully rolled out')
+                continue        
+    if old_result != track_result:
+      print("Updating status: ", track_result)
+      service.edits().tracks().update(
+                  editId=edit_id,
+                  track=TRACK,
+                  packageName=PACKAGE_NAME,
+                  body=track_result).execute()
+      commit_request = service.edits().commit(editId=edit_id, packageName=PACKAGE_NAME).execute()
+      print('Edit ', commit_request['id'], ' has been committed')    
+
+
+  except AccessTokenRefreshError:
+      raise SystemExit('The credentials have been revoked or expired, please re-run the application to re-authorize')
+
+if __name__ == '__main__':
+  main()
--- step.sh
+++ step.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-set -ex
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+echo "Updating phase release for: ${package_name}"
+if [ -z "$service_account_json_key_content" ] ; then
+    echo "Downloading credentials from remote file"
+    wget -O "${SCRIPT_DIR}/credentials.json" ${service_account_json_key_path}
+else
+    echo "Using local content credentials"
+    echo "$service_account_json_key_content" > "${SCRIPT_DIR}/credentials.json"
+fi
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+pipenv install google-api-python-client
+pipenv install oauth2client
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+pipenv run python "${SCRIPT_DIR}/rollout_update.py" "${package_name}"
+
+rm "${SCRIPT_DIR}/credentials.json"

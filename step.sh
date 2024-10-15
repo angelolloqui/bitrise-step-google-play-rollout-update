992958909a1968c55e40d4d8c3de958bb781e931
Angel Garcia <angelgarcia@Angels-MacBook-Pro.local>
2024-10-15 14:02:14 +0200

    Explict version of pyparsing

--- step.sh
+++ step.sh
@@ -12,7 +12,8 @@ else
     echo "$service_account_json_key_content" > "${SCRIPT_DIR}/credentials.json"
 fi
 
-pipenv install google-api-python-client
+pipenv install pyparsing==3.1.4
+pipenv install google-api-python-client==2.86.0
 pipenv install oauth2client
 
 echo "Running: ${SCRIPT_DIR}/rollout_update.py ${package_name} ${SCRIPT_DIR}/credentials.json ${track} ${force_rollout}"

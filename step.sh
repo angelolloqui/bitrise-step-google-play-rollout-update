4b7f1246874f4e6dbfc6755504ebabcd05dd6303
Angel G. Olloqui <angelgarcia.mail@gmail.com>
2024-10-25 11:59:56 +0200

    Added explicit urllib3 requirement

--- step.sh
+++ step.sh
@@ -15,6 +15,7 @@ fi
 pipenv install pyparsing==3.1.4
 pipenv install google-api-python-client==2.86.0
 pipenv install oauth2client
+pipenv install urllib3
 
 echo "Running: ${SCRIPT_DIR}/rollout_update.py ${package_name} ${SCRIPT_DIR}/credentials.json ${track} ${force_rollout}"
 pipenv run python "${SCRIPT_DIR}/rollout_update.py" "${package_name}" "${SCRIPT_DIR}/credentials.json" "${track}" "${force_rollout}"
